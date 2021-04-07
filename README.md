1532f36aabb5b2679ba3ce84d75285af2d6b2a99
N1flight <nadersalem123@gmail.com>
2021-04-07 17:26:42 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Note Taker
+## Description
+This is a simple note taking application where you can add, save, and delete notes
+---
+## Table of contents
+* [installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Questions](#questions)
+---
+## Installation
+npm i to install the dependencies
+---
+## Usage
+after installing dependencies run node server to run the application on localhost
+---
+## Credits
+nader salem
+---
+## License
+This application is licensed under MIT.
+---
+## Questions
+If you have any questions about this project you can contact me through [Github](https://github.com/n1flight) or send me an email at nadersalem123@gmail.com.
---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require("express")
+const path = require("path")
+const fs = require("fs")
+
+const db = require('./db/db.json')
+const uniqId = require("uniqid")
+
+
+const app = express()
+var PORT = process.env.PORT || 8000
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'))
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/notes.html'))
+})
+app.get('/api/notes', (req, res) => {
+    fs.readFile(__dirname + '/db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            thow (err)
+        } else {
+            try {
+                console.log(JSON.parse(data))
+                res.json(JSON.parse(data))
+            }
+            catch (err) {
+                console.log("Error")
+            }
+        }
+    })
+})
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body
+    const randomId = uniqId
+
+    fs.readFile(__dirname + '/db/db.json', 'utf8', (err, note) => {
+        if (err) {
+            throw err
+        }
+        let notes = JSON.parse(note)
+        req.body.id += randomId
+        notes.push(newNote)
+        
+
+        fs.writeFile(__dirname + '/db/db.json', JSON.stringify(notes), (err) => {
+            if (err) throw err
+            console.log('Saved')
+            res.json(notes)
+            console.log(notes)
+        })
+    })
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile(__dirname + '/db/db.json', 'utf8', (err, note) => {
+        if (err) {
+            throw err
+        }
+        const savedNote = JSON.parse(note)
+        const index = savedNote.findIndex((notes) => {return req.params.id === notes.id})
+        savedNote.splice(index, 1)
+        console.log(index)
+
+        fs.writeFile(__dirname + '/db/db.json', JSON.stringify(savedNote), (err) => {
+            if (err) {
+                throw err
+            }
+            console.log("Saved")
+            res.json(JSON.stringify(savedNote))
+            console.log(savedNote)
+    })
+    })
+})
+app.listen(PORT, () => {
+    console.log(`Server listening to http://localhost:${PORT}`)
+})
